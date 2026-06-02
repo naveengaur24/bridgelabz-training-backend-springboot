f63500a4c92fee309c01b520681ab6341e7dbc42
Naveen Gaur <naveengaur343@gmail.com>
2026-06-02 23:42:50 +0530

    [Naveen Gaur] Diagnostics: Added startup logging for RabbitMQ connection and simplified virtual host fallback

--- src/main/java/com/fundoonotes/fundoo_notes/FundooNotesApplication.java
+++ src/main/java/com/fundoonotes/fundoo_notes/FundooNotesApplication.java
@@ -19,4 +19,17 @@ public class FundooNotesApplication {
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
+
+	@Bean
+	public org.springframework.boot.CommandLineRunner debugRabbitMQ(org.springframework.core.env.Environment env) {
+		return args -> {
+			System.out.println("========== RABBITMQ CONFIG DIAGNOSTICS ==========");
+			System.out.println("spring.rabbitmq.host: " + env.getProperty("spring.rabbitmq.host"));
+			System.out.println("spring.rabbitmq.port: " + env.getProperty("spring.rabbitmq.port"));
+			System.out.println("spring.rabbitmq.username: " + env.getProperty("spring.rabbitmq.username"));
+			System.out.println("spring.rabbitmq.virtual-host: " + env.getProperty("spring.rabbitmq.virtual-host"));
+			System.out.println("RABBITMQ_VIRTUAL_HOST env: " + env.getProperty("RABBITMQ_VIRTUAL_HOST"));
+			System.out.println("=================================================");
+		};
+	}
 }
