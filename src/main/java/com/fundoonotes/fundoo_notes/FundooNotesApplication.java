2c2fe3ae93dc193820bbd3b3276a0f42846a5d4b
Naveen Gaur <naveengaur343@gmail.com>
2026-06-02 23:54:23 +0530

    [Naveen Gaur] fix the file fundooApplication

--- src/main/java/com/fundoonotes/fundoo_notes/FundooNotesApplication.java
+++ src/main/java/com/fundoonotes/fundoo_notes/FundooNotesApplication.java
@@ -15,21 +15,22 @@ public class FundooNotesApplication {
 
 	}
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
+	@org.springframework.beans.factory.annotation.Autowired
+	private org.springframework.core.env.Environment env;
+
+	@jakarta.annotation.PostConstruct
+	public void debugRabbitMQ() {
+		System.out.println("========== RABBITMQ CONFIG DIAGNOSTICS ==========");
+		System.out.println("spring.rabbitmq.host: " + env.getProperty("spring.rabbitmq.host"));
+		System.out.println("spring.rabbitmq.port: " + env.getProperty("spring.rabbitmq.port"));
+		System.out.println("spring.rabbitmq.username: " + env.getProperty("spring.rabbitmq.username"));
+		System.out.println("spring.rabbitmq.virtual-host: " + env.getProperty("spring.rabbitmq.virtual-host"));
+		System.out.println("RABBITMQ_VIRTUAL_HOST env: " + env.getProperty("RABBITMQ_VIRTUAL_HOST"));
+		System.out.println("=================================================");
 	}
 
 	@Bean
-	public org.springframework.boot.CommandLineRunner debugRabbitMQ(org.springframework.core.env.Environment env) {
-		return args -> {
-			System.out.println("========== RABBITMQ CONFIG DIAGNOSTICS ==========");
-			System.out.println("spring.rabbitmq.host: " + env.getProperty("spring.rabbitmq.host"));
-			System.out.println("spring.rabbitmq.port: " + env.getProperty("spring.rabbitmq.port"));
-			System.out.println("spring.rabbitmq.username: " + env.getProperty("spring.rabbitmq.username"));
-			System.out.println("spring.rabbitmq.virtual-host: " + env.getProperty("spring.rabbitmq.virtual-host"));
-			System.out.println("RABBITMQ_VIRTUAL_HOST env: " + env.getProperty("RABBITMQ_VIRTUAL_HOST"));
-			System.out.println("=================================================");
-		};
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 }
