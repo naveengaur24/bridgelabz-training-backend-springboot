b4faab8fd3d46766fa7a4f2ee056d8d3a5ffa6cb
Naveen Gaur <naveengaur343@gmail.com>
2026-06-03 00:49:39 +0530

    [Naveen Gaur]  Added..fix the file

--- src/main/java/com/fundoonotes/fundoo_notes/FundooNotesApplication.java
+++ src/main/java/com/fundoonotes/fundoo_notes/FundooNotesApplication.java
@@ -15,45 +15,6 @@ public class FundooNotesApplication {
 
 	}
 
-	@org.springframework.beans.factory.annotation.Autowired
-	private org.springframework.core.env.Environment env;
-
-	@jakarta.annotation.PostConstruct
-	public void debugRabbitMQ() {
-		System.out.println("========== ENVIRONMENT DIAGNOSTICS ==========");
-		
-		// MySQL
-		System.out.println("--- MySQL ---");
-		System.out.println("spring.datasource.url: " + env.getProperty("spring.datasource.url"));
-		System.out.println("spring.datasource.username: " + env.getProperty("spring.datasource.username"));
-		System.out.println("spring.datasource.password (obfuscated): " + getObfuscated(env.getProperty("spring.datasource.password")));
-		
-		// Redis
-		System.out.println("--- Redis ---");
-		System.out.println("spring.data.redis.host: " + env.getProperty("spring.data.redis.host"));
-		System.out.println("spring.data.redis.port: " + env.getProperty("spring.data.redis.port"));
-		System.out.println("spring.data.redis.password (obfuscated): " + getObfuscated(env.getProperty("spring.data.redis.password")));
-		
-		// RabbitMQ
-		System.out.println("--- RabbitMQ ---");
-		System.out.println("spring.rabbitmq.host: " + env.getProperty("spring.rabbitmq.host"));
-		System.out.println("spring.rabbitmq.port: " + env.getProperty("spring.rabbitmq.port"));
-		System.out.println("spring.rabbitmq.username: " + env.getProperty("spring.rabbitmq.username"));
-		System.out.println("spring.rabbitmq.virtual-host: " + env.getProperty("spring.rabbitmq.virtual-host"));
-		System.out.println("spring.rabbitmq.password (obfuscated): " + getObfuscated(env.getProperty("spring.rabbitmq.password")));
-		
-		System.out.println("=============================================");
-	}
-
-	private String getObfuscated(String val) {
-		if (val == null) return "null";
-		int len = val.length();
-		if (len > 6) {
-			return val.substring(0, 3) + "..." + val.substring(len - 3) + " (Length: " + len + ")";
-		}
-		return "*** (Length: " + len + ")";
-	}
-
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
