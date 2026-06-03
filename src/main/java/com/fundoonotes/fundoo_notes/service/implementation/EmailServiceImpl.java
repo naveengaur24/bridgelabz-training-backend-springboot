d5a715c507bc5ce0483e7456a1ab29b427406f07
Naveen Gaur <naveengaur343@gmail.com>
2026-06-03 13:50:36 +0530

    [Naveen Gaur] added..

--- src/main/java/com/fundoonotes/fundoo_notes/service/implementation/EmailServiceImpl.java
+++ src/main/java/com/fundoonotes/fundoo_notes/service/implementation/EmailServiceImpl.java
@@ -15,12 +15,13 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    @Value("${spring.mail.username}")
+    @Value("${spring.mail.properties.mail.from:${spring.mail.username}}")
     private String fromEmail;
 
     @Override
     public void sendOtpEmail(String toEmail, String otp) {
         try {
+            log.info("Sending OTP email FROM: {} TO: {}", fromEmail, toEmail);
             SimpleMailMessage message = new SimpleMailMessage();
             message.setFrom(fromEmail);
             message.setTo(toEmail);
@@ -47,6 +48,7 @@ public class EmailServiceImpl implements EmailService {
     @Override
     public void sendEmail(String to, String subject, String body) {
         try {
+            log.info("Sending email FROM: {} TO: {}", fromEmail, to);
             SimpleMailMessage message = new SimpleMailMessage();
             message.setFrom(fromEmail);
             message.setTo(to);
